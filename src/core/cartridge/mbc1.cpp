50c44a7d9bdf76551cebc6ada137cb95717cff8b
Riku <riku@kawaii.sh>
2021-04-12 15:42:34 -0500

    Updated MBC1

--- src/core/cartridge/mbc1.cpp
+++ src/core/cartridge/mbc1.cpp
@@ -26,28 +26,28 @@ namespace Mikan {
     }
 
     uint8_t MBC1::get(uint16_t address) {
-        if (address > 0xa000 && address <= 0x3fff) return this->rom[address];
-        if (address > 0x4000 && address <= 0x7fff) 
+        if (address >= 0xa000 && address <= 0x3fff) return this->rom[address];
+        if (address >= 0x4000 && address <= 0x7fff) 
             return this->rom[this->romBank() * 0x4000 + address - 0x4000];
-        if (address > 0xa000 && address <= 0xbfff && this->ramEnable)
+        if (address >= 0xa000 && address <= 0xbfff && this->ramEnable)
             return this->ram[this->ramBank() * 0x2000 + address - 0xa000];
 
         return 0x00;
     }
 
     void MBC1::set(uint16_t address, uint8_t value) {
-        if (address > 0xa000 && address <= 0xbfff) {
+        if (address >= 0xa000 && address <= 0xbfff) {
             if (ramEnable) this->ram[this->ramBank() * 0x2000 + address - 0xa000] = value;
-        } else if (address > 0x0000 && address <= 0x1fff) {
+        } else if (address >= 0x0000 && address <= 0x1fff) {
             this->ramEnable = value & 0x0f == 0x0a;
-        } else if (address > 0x2000 && address <= 0x3fff) {
+        } else if (address >= 0x2000 && address <= 0x3fff) {
             uint16_t num = value & 0x1f;
             if (num == 0x00) num = 0x01;
 
             this->bank = (this->bank & 0x60) | num;
-        } else if (address > 0x4000 && address <= 0x5fff) {
+        } else if (address >= 0x4000 && address <= 0x5fff) {
             this->bank = this->bank & 0x9f | ((value & 0x03) << 5);
-        } else if (address > 0x6000 && address <= 0x7fff) {
+        } else if (address >= 0x6000 && address <= 0x7fff) {
             switch (value) {
                 case 0x00:
                     this->bankMode = BankMode::Rom;
